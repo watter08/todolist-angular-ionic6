924313c119bd19c8ee1df5f5a41c499ff760793c
ilbert130 <ilbertca27@gmail.com>
2022-07-15 09:32:50 -0400

    firebase configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCRFS7Z_cup3SE_adKgARO2qj5UPL1vbHY",
+    authDomain: "ionic-angular-todolist-273a7.firebaseapp.com",
+    projectId: "ionic-angular-todolist-273a7",
+    storageBucket: "ionic-angular-todolist-273a7.appspot.com",
+    messagingSenderId: "147076204105",
+    appId: "1:147076204105:web:d7b3b47738734690b1e198",
+    measurementId: "G-7DZTECXMT6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCRFS7Z_cup3SE_adKgARO2qj5UPL1vbHY",
+    authDomain: "ionic-angular-todolist-273a7.firebaseapp.com",
+    projectId: "ionic-angular-todolist-273a7",
+    storageBucket: "ionic-angular-todolist-273a7.appspot.com",
+    messagingSenderId: "147076204105",
+    appId: "1:147076204105:web:d7b3b47738734690b1e198",
+    measurementId: "G-7DZTECXMT6"
+  }
 };
 
 /*
